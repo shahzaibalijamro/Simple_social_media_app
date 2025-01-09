cc34e72e41830321cf2ca02930f79510eeef48a1
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2025-01-09 07:52:42 +0500

    commands added

--- server/src/controllers/users.controllers.js
+++ server/src/controllers/users.controllers.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import User from "../models/users.models.js";
+import User from "../models/user.models.js";
 import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
 import { uploadImageToCloudinary } from "../utils/cloudinary.utils.js";
@@ -13,27 +13,18 @@ const registerUser = async (req, res) => {
     })
     const image = req.file.path;
     try {
-        const profilePicture = await uploadImageToCloudinary(image)
-        const user = new User({ userName, fullName, email, password, profilePicture })
-        const { accessToken, refreshToken } = generateAccessandRefreshTokens(user)
-        await user.save();
+        const profilePicture = await uploadImageToCloudinary(image);
+        const user = await User.create({ userName, email, password, profilePicture });
+        const { accessToken, refreshToken } = generateAccessandRefreshTokens(user);
         res
-            .cookie("refreshToken", refreshToken, { httpOnly: true, secure : true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 })
+            .cookie("refreshToken", refreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 })
             .status(201).json({
                 message: "New user created",
-                newUser: {
-                    userName: user.userName,
-                    fullName: user.fullName,
-                    profilePicture: user.profilePicture,
-                    email: user.email,
-                    _id: user._id
-                },
-                tokens: {
-                    accessToken
-                }
+                user,
+                accessToken
             })
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
         if (error.message === "Password does not meet the required criteria") {
             return res.status(400).json({ message: "Password does not meet the required criteria!" });
         }
@@ -58,7 +49,7 @@ const loginUser = async function (req, res) {
             $or: [{ email: userNameOrEmail }, { userName: userNameOrEmail }]
         });
         if (!user) return res.status(404).json({
-            message: "Invalid credentials"
+            message: "User does not exist!"
         })
         const isPasswordCorrect = await bcrypt.compare(password, user.password)
         if (!isPasswordCorrect) return res.status(401).json({
@@ -66,159 +57,16 @@ const loginUser = async function (req, res) {
         })
         const { accessToken, refreshToken } = generateAccessandRefreshTokens(user)
         res
-            .cookie("refreshToken", refreshToken, { httpOnly: true, secure : true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 })
+            .cookie("refreshToken", refreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 })
             .status(200)
             .json({
                 message: "User successfully logged in!",
-                user: {
-                    userName: user.userName,
-                    fullname: user.fullName,
-                    profilePicture: user.profilePicture,
-                    email: user.email,
-                    _id: user._id
-                },
-                tokens: {
-                    accessToken
-                }
+                user,
+                accessToken
             })
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: "An error occurred during login" });
-    }
-}
-
-
-//update user data
-
-const updateFullNameOrUserName = async (req, res) => {
-    const { userName, fullName } = req.body;
-    const decoded = req.user;
-    const accessToken = req.tokens?.accessToken;
-    try {
-        if (!userName || !fullName) {
-            return res.status(400).json({
-                message: "Username and fullname are required!"
-            })
-        }
-        const user = await User.findById(
-            decoded._id || decoded.id).select('-password');
-        if (!user) {
-            return res.status(404).json({
-                message: "User does not exist!"
-            })
-        }
-        const lowerCaseUserName = userName.toLowerCase()
-        if (user.userName !== lowerCaseUserName) {
-            const isUserNameTaken = await User.findOne({ userName: lowerCaseUserName });
-            if (isUserNameTaken) {
-                return res.status(400).json({
-                    message: "This username is already taken, try another one!",
-                    taken: true
-                })
-            }
-        }
-        if (user.fullName !== fullName || user.userName !== lowerCaseUserName) {
-            const update = await User.findByIdAndUpdate(
-                user._id,
-                {
-                    fullName,
-                    userName: lowerCaseUserName
-                },
-                { new: true }).select('-password');
-            return res.status(200).json({
-                message: "Username and fullname updated!",
-                user: update,
-                ...(accessToken && { accessToken })
-            })
-        }
-        return res.status(200).json({
-            message: "Username and fullname are already up-to-date!",
-            user,
-            ...(accessToken && { accessToken })
-        })
-    } catch (error) {
-        console.log(error.message || error);
-        res.status(500).json({
-            message: "Something went wrong!"
-        })
-    }
-}
-
-
-const resetPassword = async (req, res) => {
-    try {
-        const { currentPassword, newPassword } = req.body.data;
-        const decoded = req.user;
-        const accessToken = req.tokens?.accessToken;
-        if (!currentPassword || !newPassword) {
-            return res.status(400).json({
-                message: "Insufficient data recieved!"
-            })
-        }
-        const user = await User.findOne({ email: decoded.email })
-        if (!user) return res.status(404).json({
-            message: "User does not exist!"
-        })
-        const checkPassword = await bcrypt.compare(currentPassword, user.password);
-        if (!checkPassword) return res.status(400).json({
-            isPasswordCorrect: false,
-            message: "Incorrect Password"
-        })
-        user.password = newPassword
-        await user.save()
-        res.status(200).json({
-            isPasswordCorrect: true,
-            message: "Password updated",
-            ...(accessToken && { accessToken })
-        })
-    } catch (error) {
-        console.log(error.message || error);
-        if (error.message === "Password does not meet the required criteria") {
-            return res.status(400).json({
-                message: "Password does not meet the required criteria!"
-            })
-        }
-        res.status(400).json({
-            message: "Something went wrong"
-        })
-    }
-}
-
-
-const updateProfilePicture = async (req, res) => {
-    const decoded = req.user;
-    const accessToken = req.tokens?.accessToken;
-    if (!req.file) return res.status(400).json({
-        message: "No file found"
-    })
-    if (!decoded) return res.status(400).json({
-        message: "No token found!"
-    })
-    const image = req.file.path;
-    try {
-        const doesUserExist = await User.findById(decoded._id || decoded.id);
-        if (!doesUserExist) {
-            res.status(404).json({
-                message: "User does not exist!"
-            })
-        }
-        const url = await uploadImageToCloudinary(image);
-        if (!url) {
-            return res.status(500).json({
-                message: "Could not upload the image!"
-            })
-        }
-        const updated = await User.findByIdAndUpdate(doesUserExist._id, { profilePicture: url }, { new: true }).select('-password')
-        res.status(200).json({
-            message: "Profile Picture updated!",
-            user: updated,
-            ...(accessToken && { accessToken })
-        })
-    } catch (error) {
-        console.log(error, "==> this");
-        return res.status(500).json({
-            message: "Something went wrong!"
-        })
     }
 }
 
@@ -232,9 +80,7 @@ const deleteUser = async (req, res) => {
         const decodedToken = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
         session = await mongoose.startSession();
         session.startTransaction();
-        const [deleteUser] = await Promise.all([
-            User.findByIdAndDelete(decodedToken._id,{session})
-        ])
+        const deleteUser = await User.findByIdAndDelete(decodedToken._id, { session });
         if (!deleteUser) {
             await session.abortTransaction();
             return res.status(404).json({
@@ -242,35 +88,35 @@ const deleteUser = async (req, res) => {
             })
         }
         await session.commitTransaction();
-        res.clearCookie("refreshToken", { 
-            httpOnly: true, 
-            secure : true,
-            sameSite: 'None', 
+        res.clearCookie("refreshToken", {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'None',
             maxAge: 0,
             path: '/',
         });
         return res.status(200).json({ message: "User deleted!" });
     } catch (error) {
-        if(session) await session.abortTransaction()
+        if (session) await session.abortTransaction()
         if (error.name === "JsonWebTokenError" || error.name === "TokenExpiredError") {
             return res.status(401).json({ message: "Invalid or expired token" });
         }
         console.log(error);
         return res.status(500).json({ message: "Error occurred while deleting the user" });
-    }finally{
-        if(session) await session.endSession()
+    } finally {
+        if (session) await session.endSession()
     }
 };
 
 
-const getAllUsers = async (req,res) => {
+const getAllUsers = async (req, res) => {
     const page = req.query?.page || 1;
     const limit = req.query?.limit || 10;
     console.log(page);
     const skip = (+page - 1) * +limit;
     try {
         const allUsers = await User.find({}).skip(skip).limit(limit);
-        if(allUsers.length === 0) return res.status(200).json({
+        if (allUsers.length === 0) return res.status(200).json({
             message: "no data left!"
         })
         res.status(200).json(allUsers)
@@ -283,4 +129,4 @@ const getAllUsers = async (req,res) => {
 }
 
 
-export { registerUser, getAllUsers ,loginUser, updateFullNameOrUserName, resetPassword, updateProfilePicture, deleteUser }
+export { registerUser, getAllUsers, loginUser, deleteUser }

a8ce7dcfbe2b5f3e2502c632e5ac3be0f446b913
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2025-01-09 15:38:20 +0500

    commands added

---
+++ client/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <Button>Hello</Button>
+    </div>
+  )
+}
+
+export default page

5164664e5b5a23fe64f7ef87db81676d123add2d
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2025-01-11 09:01:47 +0500

    commands added

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -11,10 +11,14 @@ export default function Login() {
     const [userNameOrEmail,setUserNameOrEmail] = useState("");
     const [password,setPassword] = useState("");
     const loginUser = async () => {
-        const {data} = await axios.post("/login",{
-            userNameOrEmail,
-            password
-        })
+        try {
+            const {data} = await axios.post("/login",{
+                userNameOrEmail,
+                password
+            })
+        } catch (error) {
+            console.log(error);
+        }
     }
     return (
         <div className="w-full px-3 h-[90vh] flex justify-center items-center">
@@ -23,6 +27,7 @@ export default function Login() {
                     <CardTitle className="text-3xl text-[#1e40af]">Login</CardTitle>
                     <CardDescription>Enter your information to Login</CardDescription>
                 </CardHeader>
+                <form onSubmit={loginUser}>
                 <CardContent className="space-y-4">
                     <div className="space-y-2">
                         <Label htmlFor="username-or-email">Username or Email</Label>
@@ -32,7 +37,7 @@ export default function Login() {
                         <Label htmlFor="password">Password</Label>
                         <Input id="password" onChange={(e) => setPassword(e.target.value)} placeholder="password" type="password" required minLength={8}/>
                     </div>
-                    <Button className="w-full hover:bg-[#3655bd] bg-[#1e40af]">Login</Button>
+                    <Button type="submit" className="w-full hover:bg-[#3655bd] bg-[#1e40af]">Login</Button>
                     <div className="flex w-full justify-center gap-x-2 items-center">
                         <h1>New here?</h1>
                         <Link href={'/register'}>
@@ -40,6 +45,7 @@ export default function Login() {
                         </Link>
                     </div>
                 </CardContent>
+                </form>
             </Card>
         </div>
     )

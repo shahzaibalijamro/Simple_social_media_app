6a26587e57ec00580fcf8218090886c54258f24e
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2025-01-13 13:42:45 +0500

    commands added

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,11 +1,45 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
+import axios from "@/config/axiosConfig";
+import { useRouter } from 'next/navigation';
+import Login from '../login';
+import { Progress } from '@/components/ui/progress';
 
-const Login = () => {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  )
+interface tokenState {
+    token: {
+        accessToken: string,
+    }
 }
 
-export default Login
+const login = () => {
+    const router = useRouter();
+    const [loading, setLoading] = useState(false);
+    const [loadingVal, setLoadingVal] = useState(33);
+    const accessToken = useSelector((state: tokenState) => state.token.accessToken);
+    const authenticateUserState = async () => {
+        setLoadingVal(90);
+        try {
+            const { data } = await axios.post("/api/v1/protected");
+            console.log(data);
+            router.replace("/");
+        } catch (error: any) {
+            console.log(error);
+            const errorMsg = error.response?.data.message;
+            if (errorMsg === "Access token and refresh token are required! Please log in again.") {
+                setLoading(true)
+            }
+        }
+    }
+    useEffect(() => {
+        authenticateUserState();
+    }, [])
+    console.log(accessToken);
+    return (
+        <div>
+            {loading ? <Login /> : <div className='w-full h-[90vh] flex justify-center items-center max-w-[200px] mx-auto px-4'><Progress value={loadingVal} /></div>}
+        </div>
+    )
+}
+
+export default login
--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -1,33 +1,45 @@
 "use client"
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux';
-import axios from "@/config/axiosConfig"
+import axios from "@/config/axiosConfig";
+import { useRouter } from 'next/navigation';
+import Register from '../register';
+import { Progress } from '@/components/ui/progress';
+
 interface tokenState {
     token: {
         accessToken: string,
     }
 }
 
-const page = () => {
+const register = () => {
+    const router = useRouter();
+    const [loading, setLoading] = useState(false);
+    const [loadingVal, setLoadingVal] = useState(33);
     const accessToken = useSelector((state: tokenState) => state.token.accessToken);
-    const authenticateUserState = async () =>{
+    const authenticateUserState = async () => {
+        setLoadingVal(90);
         try {
-            const {data} = await axios.post("/api/v1/protected");
+            const { data } = await axios.post("/api/v1/protected");
             console.log(data);
-        } catch (error) {
+            router.replace("/");
+        } catch (error: any) {
             console.log(error);
+            const errorMsg = error.response?.data.message;
+            if (errorMsg === "Access token and refresh token are required! Please log in again.") {
+                setLoading(true)
+            }
         }
     }
-    useEffect(()=>{
+    useEffect(() => {
         authenticateUserState();
-    },[])
+    }, [])
     console.log(accessToken);
     return (
         <div>
-            <h1>{accessToken && accessToken}</h1>
-            <h1>Checking</h1>
+            {loading ? <Register /> : <div className='w-full h-[90vh] flex justify-center items-center max-w-[200px] mx-auto px-4'><Progress value={loadingVal} /></div>}
         </div>
     )
 }
 
-export default page
+export default register

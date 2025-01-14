5db0e1f8cafcf379459478a86ddfed461439285b
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2025-01-14 10:57:04 +0500

    like post

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -7,6 +7,7 @@ import { Progress } from '@/components/ui/progress';
 import Head from 'next/head';
 import { toast, Toaster } from 'sonner';
 import { useSelector } from 'react-redux';
+import { useRouter } from 'next/navigation';
 
 interface tokenState {
   token: {
@@ -37,6 +38,7 @@ interface userState {
 }
 
 const Home = () => {
+  const router = useRouter()
   const [loading, setLoading] = useState(true);
   const [textInput, setTextInput] = useState("");
   const [posts, setPosts] = useState<singlePost[]>([]);
@@ -67,8 +69,8 @@ const Home = () => {
       return toast("Unauthorized!", {
         description: `You need to log in to add a post. Please log in and try again.`,
         action: {
-          label: "OK",
-          onClick: () => console.log("ok"),
+          label: "Login",
+          onClick: () => router.push("/login"),
         },
       })
     }
@@ -101,7 +103,62 @@ const Home = () => {
       setPosts([...posts, post]);
     } catch (error) {
       console.log(error);
+      toast("Something went wrong!", {
+        description: `Please try again later.`,
+        action: {
+          label: "Retry",
+          onClick: () => addPost(),
+        },
+      })
     }
+  }
+  const likePost = async (id:string,index:number) => {
+    console.log(id)
+    if (!accessToken) {
+      return toast("Unauthorized!", {
+        description: `You need to log in to like a post. Please log in and try again.`,
+        action: {
+          label: "Login",
+          onClick: () => router.push("/login"),
+        },
+      })
+    }
+    try {
+      const {data} = await axios.post(`/api/v1/post/${id}`,{},{
+        headers: {
+          'Authorization' : `Bearer ${accessToken}`
+        }
+      })
+      console.log(data);
+      const confirmationMessage = data.message;
+      if (confirmationMessage === "Post Unliked") {
+        const {unLike} = data;
+        posts[index].likes = unLike.likes;
+        setPosts([...posts]);
+        return toast("Post Unliked!", {
+          action: {
+            label: "Undo",
+            onClick: () => likePost(id,index),
+          },
+        });
+      }
+      if (confirmationMessage === "Post liked") {
+        const {like} = data;
+        posts[index].likes = like.likes;
+        setPosts([...posts]);
+        return toast("Post liked!", {
+          action: {
+            label: "Undo",
+            onClick: () => likePost(id,index),
+          },
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  const commentOnPost = async (id:string) => {
+    console.log(id,"Comment")
   }
   return (
     <>
@@ -113,8 +170,8 @@ const Home = () => {
       <InputForm setTextInput={setTextInput} mediaRef={mediaRef} addPost={addPost} />
       <div className='h-6 w-full'></div>
       <div>
-        {posts.length > 0 && !loading ? posts.map((item: singlePost) => {
-          return <Card username={item.userId.userName} comments={item.comments.length} image={item.media} likes={item.likes.length} time={item.createdAt} text={item.content} />
+        {posts.length > 0 && !loading ? posts.map((item: singlePost,index:number) => {
+          return <Card key={item._id} likePost={likePost} commentOnPost={commentOnPost} index={index} item={item} />
         }) : posts.length === 0 && !loading ? <div className='w-full justify-center items-center mt-4 flex'><h1>No posts found!</h1></div> : posts.length === 0 && loading ? <div className='max-w-[200px] mx-auto px-4 justify-center items-center mt-4 flex'><Progress value={loadingVal} /></div> : <></>}
       </div>
       <div className='h-6 w-full'></div>
--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -6,15 +6,26 @@ import Comment from "@/assets/chat.png"
 import Image from 'next/image';
 import BlueLike from "@/assets/like (1).png";
 interface Card {
-    username: string,
-    image: string,
-    likes: number,
-    comments: number,
-    time: string,
-    text: string
+    item: {
+        userId: {
+            userName: string;
+        };
+        _id: string;
+        content: string;
+        createdAt: string;
+        updatedAt: string;
+        media: string;
+        likes: any[];
+        comments: any[];
+        __v: number;
+    }
+    likePost: (id:string,index:number) => void,
+    commentOnPost: (id:string,index:number) => void,
+    index: number
 }
-const Card = ({ username, image, likes, comments, time, text }: Card) => {
-    const createdDate = new Date(time);
+
+const Card = ({ item,index, likePost, commentOnPost }: Card) => {
+    const createdDate = new Date(item.createdAt);
     const now = Date.now();
     const diffInMs = now - createdDate.getTime();
     const diffInHours = Math.floor(diffInMs / (1000 * 60 * 60));
@@ -51,36 +62,36 @@ const Card = ({ username, image, likes, comments, time, text }: Card) => {
             <div className='flex gap-x-3 mb-3 justify-start items-center'>
                 <div>
                     <Avatar className='w-9 h-9'>
-                        <AvatarFallback>{username[0] + username[1]}</AvatarFallback>
+                        <AvatarFallback>{item.userId.userName[0] + item.userId.userName[1]}</AvatarFallback>
                     </Avatar>
                 </div>
                 <div>
-                    <div><h1 className='font-medium'>{username}</h1></div>
+                    <div><h1 className='font-medium'>{item.userId.userName}</h1></div>
                     <div><h1 className='text-sm text-gray-600 font-medium'>{calculateDays(diffInHours)} ago</h1></div>
                 </div>
             </div>
-            {text && <div className='text-start mb-2 px-1 text-gray-600'>
-                <h1 className='text-[15px]'>{text}</h1>
+            {item.content && <div className='text-start mb-2 px-1 text-gray-600'>
+                <h1 className='text-[15px]'>{item.content}</h1>
             </div>}
-            {image && <div className='w-full bg-gray-200 rounded-xl flex justify-center items-center mb-2'>
-                <img src={image} alt="" />
+            {item.media && <div className='w-full bg-gray-200 rounded-xl flex justify-center items-center mb-2'>
+                <img src={item.media} alt="" />
             </div>}
             <div className='flex justify-between mb-2 items-center'>
                 <div className='flex justify-start ps-2 gap-x-1 items-center'>
                     <Image src={BlueLike} width={19} alt='reaction' />
-                    <h1 className='text-sm text-gray-600 font-medium'>{likes}</h1>
+                    <h1 className='text-sm text-gray-600 font-medium'>{item.likes.length}</h1>
                 </div>
                 <div className='flex justify-center pe-2 gap-x-2 items-center'>
-                    <h1 className='text-sm text-gray-600 font-medium'>{comments}</h1>
+                    <h1 className='text-sm text-gray-600 font-medium'>{item.comments.length}</h1>
                     <h1 className='text-sm text-gray-600 font-medium'>Comments</h1>
                 </div>
             </div>
             <div className='flex justify-center w-full gap-x-2 items-center'>
-                <Button className='bg-gray-200 hover:bg-gray-300 flex justify-center items-center flex-1  font-medium text-base text-[#1e40af]'>
+                <Button onClick={() => likePost(item._id,index)} className='bg-gray-200 hover:bg-gray-300 flex justify-center items-center flex-1  font-medium text-base text-[#1e40af]'>
                     <Image src={Like} width={20} alt='like' />
                     Like
                 </Button>
-                <Button className='bg-gray-200 hover:bg-gray-300 flex justify-center items-center flex-1  font-medium text-base text-[#1e40af]'>
+                <Button onClick={() => commentOnPost(item._id,index)} className='bg-gray-200 hover:bg-gray-300 flex justify-center items-center flex-1  font-medium text-base text-[#1e40af]'>
                     <Image src={Comment} width={20} alt='like' />
                     Comment
                 </Button>
